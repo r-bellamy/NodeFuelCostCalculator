af20f0244b0f298b77c14bbdc5de3f155da1d33e
r-bellamy <84311073+r-bellamy@users.noreply.github.com>
2024-03-22 15:30:09 +0000

    Add files via upload

---
+++ fuelcostcalculator.js
@@ -0,0 +1,26 @@
+//const prompt = require('prompt-sync')();
+var url = require('url');
+
+const calculateFuelCost = (miles) => {
+	const mpg = 30;
+	const costPerLitre = 142;
+	const gallonInLitres = 4.54609;
+
+
+	const milesPerLitre = mpg / gallonInLitres;
+	const litreConsumption = miles / milesPerLitre;
+	const cost = litreConsumption * costPerLitre / 100;
+	return cost.toFixed(2);
+}
+
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var q = url.parse(req.url, true).query;
+	
+	res.write(`Total cost: &pound;${calculateFuelCost(q.miles)}`);
+	res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
